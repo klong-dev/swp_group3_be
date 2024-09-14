fa47b0b736e4545da019a9a80defa9900c181018
viethoang1520 <vhd151020@gmail.com>
2024-09-15 01:58:05 +0700

    create backend source code structure

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+require('dotenv').config();
+
+const db = require("./config/db");
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+db.connect();
+routes(app);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
+});
