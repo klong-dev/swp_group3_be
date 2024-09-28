ce906c6fe4c3da35178936f2f65a3e8457fa32d4
Hoàng Kim Long <longhoang8204@gmail.com>
2024-09-28 15:33:21 +0700

    chore: merge code 28/9/24

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const google = require('./account/google-auth')
 
 function routes(app) {
   app.use('/auth/google', google);
-
+  app.use('/auth/google/callback', google); 
 }
 
 module.exports = routes;
