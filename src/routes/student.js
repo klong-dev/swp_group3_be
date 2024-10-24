734d8af5154e7909ffbf267c5684298446a35151
viethoang1520 <vhd151020@gmail.com>
2024-10-25 00:06:30 +0700

    feat: apply mentor

--- src/routes/student.js
+++ src/routes/student.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const StudentController = require('../app/controllers/StudentController');
 const Auth = require('../middleware/AuthenticateJWT')
 router.post('/valid', Auth, StudentController.validStudent);
+router.post('/apply-mentor', Auth, StudentController.applyToBeMentor);
+
 
 module.exports = router
