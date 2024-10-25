9227c252dfe80e7c81c03804ce5e0b7cc8d72f88
viethoang1520 <vhd151020@gmail.com>
2024-10-25 20:46:04 +0700

    feat: get applying list

--- src/routes/student.js
+++ src/routes/student.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const StudentController = require('../app/controllers/StudentController');
 const Auth = require('../middleware/AuthenticateJWT')
+router.get('/applying', Auth, StudentController.applyingMentors);
 router.post('/valid', Auth, StudentController.validStudent);
 router.post('/apply-mentor', Auth, StudentController.applyToBeMentor);
 
