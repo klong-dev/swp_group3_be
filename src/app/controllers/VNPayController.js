2252a2f7f1618efdfd128a1fbf704c3a007cbc70
Hoàng Kim Long <longhoang8204@gmail.com>
2024-10-30 01:42:46 +0700

    feat: add get result payment

--- src/app/controllers/VNPayController.js
+++ src/app/controllers/VNPayController.js
@@ -5,6 +5,7 @@ const moment = require("moment-timezone");
 const Item = require("../models/Item");
 const Mentor = require("../models/Mentor");
 const Student = require("../models/Student");
+const Donate = require("../models/Donate");
 
 class VNPayController {
     sortObject = (obj) => {
@@ -21,6 +22,53 @@ class VNPayController {
             sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
         }
         return sorted;
+    }
+
+    paymentResult = async (req, res) => {
+        try {
+            const { vnp_OrderInfo, vnp_TransactionStatus } = req.query;
+            const [itemId, mentorId, studentId] = vnp_OrderInfo.split("_").slice(1);
+            const item = await Item.findByPk(itemId);
+            if (!item) {
+                return res.status(404).json({ error_code: 2, message: "Item not found" });
+            }
+            const mentor = await Mentor.findByPk(mentorId);
+            if (!mentor) {
+                return res.status(404).json({ error_code: 2, message: "Mentor not found" });
+            }
+            const student = await Student.findByPk(studentId);
+            if (!student) {
+                return res.status(404).json({ error_code: 2, message: "Student not found" });
+            }
+            if (vnp_TransactionStatus === "00") {
+                await Donate.create({
+                    itemId: item.id,
+                    mentorId: mentor.accountId,
+                    studentId: student.accountId,
+                    amount: 1,
+                    status: 1
+                });
+                return res.status(200).json({ error_code: 0, message: "Payment success" });
+            } else {
+                const status_error = {
+                    "01": "Bank system error",
+                    "02": "Giao dịch chưa hoàn thành",
+                    "03": "Giao dịch bị lỗi",
+                    "04": "Giao dịch đảo (Khách hàng đã bị trừ tiền tại Ngân hàng nhưng GD chưa thành công ở VNPAY)",
+                    "05": "VNPAY đang xử lý giao dịch này (GD hoàn tiền)",
+                    "06": "VNPAY đã gửi yêu cầu hoàn tiền sang Ngân hàng (GD hoàn tiền)",
+                    "07": "Giao dịch bị nghi ngờ gian lận",
+                    "08": "Giao dịch quá thời gian thanh toán",
+                    "09": "GD Hoàn trả bị từ chối",
+                    "10": "Đã giao hàng",
+                    "11": "Giao dịch bị hủy",
+                    "12": "Giao dịch đã được thanh quyết toán cho merchant",
+                }
+                return res.status(400).json({ error_code: 3, message: "Payment failed", status_error: status_error[vnp_TransactionStatus] });
+            }
+        } catch (error) {
+            return res.status(500).json({ error_code: 5, message: "Internal server error", error: error.message });   
+        }
     }
 
     createPaymentUrl = async (req, res) => {
@@ -44,7 +92,7 @@ class VNPayController {
             vnp_Amount: Math.round(item.price * 100), // Chuyển đổi sang đơn vị VND
             vnp_CurrCode: "VND",
             vnp_TxnRef: `VNP${moment().tz("Asia/Ho_Chi_Minh").format("YYYYMMDDHHmmss")}`,
-            vnp_OrderInfo: `payment_${item.id}_${mentor.id}_${student.id}`,
+            vnp_OrderInfo: `payment_${item.id}_${mentor.accountId}_${student.accountId}`,
             vnp_Locale: "vn",
             vnp_BankCode: "NCB",
             vnp_ReturnUrl: process.env.CLIENT_URL + "/payment/result",
