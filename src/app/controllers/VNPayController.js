6fbe6dadcbc9e16e8968263674d1ad5678caf61c
Hoàng Kim Long <longhoang8204@gmail.com>
2024-10-30 02:40:58 +0700

    fix: change return url

--- src/app/controllers/VNPayController.js
+++ src/app/controllers/VNPayController.js
@@ -95,7 +95,7 @@ class VNPayController {
             vnp_OrderInfo: `payment_${item.id}_${mentor.accountId}_${student.accountId}`,
             vnp_Locale: "vn",
             vnp_BankCode: "NCB",
-            vnp_ReturnUrl: process.env.CLIENT_URL + "/payment/result",
+            vnp_ReturnUrl: process.env.CLIENT_URL + "/donate-process",
             vnp_IpAddr: "14.186.73.156", // Sử dụng địa chỉ IP cụ thể
             vnp_CreateDate: moment().tz("Asia/Ho_Chi_Minh").format("YYYYMMDDHHmmss"),
             vnp_OrderType: "topup",
