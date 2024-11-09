3372a3ae5638106f0ef2750d857bec13dfbe0e26
nhathao04 <nhathao06012004.bl@gmail.com>
2024-11-09 08:59:11 +0700

    chore: update AdminController

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -30,7 +30,7 @@ router.get('/search-mentor-by-id', Auth, AdminController.searchMentorByMentorId)
 router.get('/search-student-by-name', Auth, AdminController.searchStudentByName);
 router.get('/search-student-by-id', Auth, AdminController.searchStudentByStudentId);
 
-router.post('/delete-skill', AdminController.deleteSkill);
-router.post('/update-skill', AdminController.updateSKill);
+router.post('/delete-skill', Auth, AdminController.deleteSkill);
+router.post('/update-skill', Auth, AdminController.updateSKill);
 
 module.exports = router
